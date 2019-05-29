12e368176d860e68c39f16afb09fe15506d882b9
Ian Jones <ian@egghead.io>
2019-05-28 21:10:40 -0400

    revert start file

--- lesson-usesubscription-urql/src/App.start.js
+++ lesson-usesubscription-urql/src/App.start.js
@@ -1,30 +1,13 @@
 import React from 'react'
 import CommentInput from './components/CommentInput'
 import CommentList from './components/CommentList'
-import {useSubscription} from 'urql'
-
-const commentSubscriptionQuery = `
-  subscription subscribeToComments($course_slug: String!) {
-    comments(where: {course_slug: {_eq: $course_slug}}){
-      text
-    }
-  }
-`
 
 function App() {
   const course_slug = 'usesubscription-example'
-  const [result] = useSubscription({
-    query: commentSubscriptionQuery,
-    variables: {course_slug},
-  })
-
-  if (result.data === undefined) {
-    return <div>No comments</div>
-  }
 
   return (
     <div>
-      <CommentList comments={result.data.comments} />
+      <CommentList />
       <CommentInput />
     </div>
   )
